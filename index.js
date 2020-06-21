fc036c6bb94c67a277a899aafbe318842b9f0b3d
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-06-21 22:36:47 +0100

    chore:(test deleting unwanted  files)

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
+const { exec } = require('child_process');
 const inquirer = require('inquirer');
+const { stderr, stdout } = require('process');
+
+
 
 inquirer.prompt([{
     name: "delete",
     type: 'checkbox',
     message: 'Cleanup create-react-app',
-    choices: ['App.js', 'logo.svg', 'App.css']
+    choices: ['App.css', 'logo.svg', 'stupid.css']
 }
 
 ]).then((answers) => {
-    console.log(answers.delete);
-});
+    
+    // console.log(answers.delete);
+    answers.delete.forEach((answer)=>{
+        
+        if(answer === 'stupid.css'){
+        exec(`rm ${process.cwd()}/stupid.css`,(err,stdout, stderr) =>{
+            if(err){
+                console.log(err)
+            }else{
+                console.log('deleted stupid file');
+                
+            }
+        })
+        }else{
+            return;
+        }
+    })
+    
+}).catch(err => console.log(err))
