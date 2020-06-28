0e7db32f3143181c2579b9633ca322c640007396
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-06-28 23:11:16 +0100

    chore:(Ask for files to be deleted)

--- index.js
+++ index.js
@@ -1,90 +1,98 @@
 #!/usr/bin/env node
 
 const {exec} = require('child_process');
+const inquirer = require("inquirer");
 
 exec('npm bin', {cwd: __dirname}, (err, stdout, stderr) => {
-  exec(`rm ${process.cwd()}/src/App.css ${process.cwd()}/src/App.test.js ${process.cwd()}/src/index.css ${process.cwd()}/src/logo.svg ${process.cwd()}/src/serviceWorker.js ${process.cwd()}/src/index.js ${process.cwd()}/src/App.js `, {cwd: __dirname}, (err, stdout, stderr) => {
-    if(err){
-      console.error(err);  
-    }else{
-      exec(`npm run clear`, {cwd: __dirname},(err, stdout, stderr)=>{
-        if (err) {
-          console.error(err);
-        } else {
-            console.log('🚀');
-        }
-      })
-
+inquirer
+.prompt([
+  {
+    name: "delete",
+    type: "checkbox",
+    message: "cleanup create-react-app",
+    choices: ["App.js", "logo.svg", "index.css", "App.css", "App.test.js", "serviceWorker.js", "index.js"],
+  },
+])
+.then((answers) => {
+  // console.log(answers.delete);
+  if(err){
+    console.log('Error' + err)
+  }else{
+    answers.delete.forEach((answer) => {
+      switch(answer){
+        case "App.js":
+          exec(`rm ${process.cwd()}/src/App.js`, (err, stdout, stderr) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("deleted app.js");
+            }
+          });
+        break;
+        case "logo.svg":
+          exec(`rm ${process.cwd()}/src/logo.svg`, (err, stdout, stderr) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("deleted logo.svg");
+            }
+          });
+        break;
+        case "App.css":
+          exec(`rm ${process.cwd()}/src/App.css`, (err, stdout, stderr) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("deleted app.css");
+            }
+          });
+        break;
+        case "index.js":
+          exec(`rm ${process.cwd()}/src/index.js`, (err, stdout, stderr) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("deleted index.js");
+            }
+          });
+        break;
+        case "index.css":
+          exec(`rm ${process.cwd()}/src/index.css`, (err, stdout, stderr) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("deleted index.css");
+            }
+          });
+        break;
+        case "App.test.js":
+          exec(`rm ${process.cwd()}/src/App.test.js`, (err, stdout, stderr) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("deleted app.test.js");
+            }
+          });
+        break;
+        case "serviceWorker.js":
+          exec(`rm ${process.cwd()}/src/serviceWorker.js`, (err, stdout, stderr) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("deleted serviceWorker.js");
+            }
+          });
+        break;
+      }
+    });
+  }
+  exec(`npm run clear`, {cwd: __dirname},(err, stdout, stderr)=>{
+    if (err) {
+      console.error(err);
+    } else {
+        console.log('React app sucessfully cleaned up');
     }
   })
+})
+.catch((err) => console.log(err));
 });
-
-// const { exec } = require("child_process");
-// const inquirer = require("inquirer");
-// const { stderr, stdout } = require("process");
-
-// inquirer
-//   .prompt([
-//     {
-//       name: "delete",
-//       type: "checkbox",
-//       message: "Cleanup create-react-app",
-//       choices: ["App.css", "logo.svg", "stupid.css", "App.js"],
-//     },
-//   ])
-//   .then((answers) => {
-//     // console.log(answers.delete);
-//     answers.delete.forEach((answer) => {
-//       if (answer === "stupid.css") {
-//         exec(`rm ${process.cwd()}/stupid.css`, (err, stdout, stderr) => {
-//           if (err) {
-//             console.log(err);
-//           } else {
-//             console.log("deleted stupid file");
-//           }
-//         });
-//       } else if (answer == "App.css") {
-//         exec(`rm ${process.cwd()}/App.css`, (err, stdout, stderr) => {
-//           if (err) {
-//             console.log(err);
-//           } else {
-//             console.log("deleted stupid file");
-//           }
-//         });
-//       } else if (answer == "logo.svg") {
-//         exec(`rm ${process.cwd()}/logo.svg`, (err, stdout, stderr) => {
-//           if (err) {
-//             console.log(err);
-//           } else {
-//             console.log("deleted stupid file");
-//           }
-//         });
-//       } else if (answer == "App.js") {
-//         exec(
-//           `echo "import React from 'react';\nimport ReactDOM from 'react-dom';\nimport App from './App';\nReactDOM.render(<App />, document.getElementById('root'));" >> ${process.cwd()}/App.js`,
-//           (err, stdout, stderr) => {
-//             if (err) {
-//               console.log(err);
-//             } else {
-//               console.log("deleted stupid file");
-//             }
-//           }
-//         );
-//       } else {
-//         return "errr";
-//       }
-//     });
-//   })
-//   .catch((err) => console.log(err));
-
-// const {exec} = require('child_process');
-
-// exec('npm bin', {cwd: __dirname}, (err, stdout, stderr) => {
-  // exec(`echo import React from 'react';\nimport ReactDOM from 'react-dom';\nimport App from './App';\nReactDOM.render(<App />, document.getElementById('root')); > ${process.cwd()}/index.jsx `, {cwd: __dirname}, (err, stdout, stderr) => {
-  //   if (err) {
-  //     console.error(err);
-  //   } else {
-  //       console.log('🚀');
-  //   }
-  // });
-// });
--- src/App.js
+++
@@ -1,1 +0,0 @@
-import React, { Component } from 'react';  class App extends Component {  render() {     return (       <h1>Hello World</h1>     );   }  }  export default App;
